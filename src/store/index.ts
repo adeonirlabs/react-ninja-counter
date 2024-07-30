50a7d483fa4893de09bdc5af78067f11809cd964
Adeonir Kohl <adeonir@gmail.com>
2024-07-30 10:00:24 -0300

    Add types to store state and dispatch

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,3 +7,6 @@ export const store = configureStore({
     counter: counterReducer,
   },
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
