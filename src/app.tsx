542a84ca68e4b8f67a7f6371ceb46118b3d2b41a
Adeonir Kohl <adeonir@gmail.com>
2024-07-31 15:57:33 -0300

    Remove state management from app and add counters-list

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,9 @@
-import { useDispatch, useSelector } from 'react-redux'
-
-import { Counter } from '~/components/counter'
-import { decrement, increment } from '~/slices/counter'
-import type { AppDispatch, RootState } from '~/store'
+import { CounterList } from './components/app/counters-list'
 
 export function App() {
-  const counter = useSelector((state: RootState) => state.counter.value)
-  const dispatch: AppDispatch = useDispatch()
-
-  const handleDecrement = () => dispatch(decrement())
-  const handleIncrement = () => dispatch(increment())
-
   return (
     <main className="flex h-screen items-center justify-center">
-      <Counter counter={counter} decrement={handleDecrement} increment={handleIncrement} />
+      <CounterList />
     </main>
   )
 }
